a7e88a0cb2c5c784812da6da2a849075b64d84fe
aleks <newacb1979@mail.ru>
2024-11-01 09:09:27 +1000

    added handling of the forced program termination exception

--- parser_with_db/parser.py
+++ parser_with_db/parser.py
@@ -9,6 +9,9 @@ from parser_with_db.src.config import (PERIOD_BETWEEN_REQUEST, logger,
 from parser_with_db.src.requests import request_share_sber
 from parser_with_db.src.processor import handle_call_chain
 
+# Остановка приложения через Ctrl+C
+stop_event = asyncio.Event()
+
 
 async def main():
     """Обрабатывает цикл вызовов
@@ -24,7 +27,7 @@ async def main():
             try:
                 await handle_call_chain(name_table, url, template)
             except (aiohttp.ClientError, IndexError, KeyError,
-                    TypeError, ValueError, aiosqlite.Error):
+                    TypeError, ValueError, aiosqlite.Error, AttributeError):
                 logger.error("Ошибка выполнения процесса")
             finally:
                 logger.info("Цикл обработки запроса и записи в БД завершен\n")
@@ -33,5 +36,12 @@ async def main():
 
 
 if __name__ == "__main__":
-    while True:
-        asyncio.run(main())
+    while not stop_event.is_set():
+        try:
+            asyncio.run(main())
+        except (aiohttp.ClientError, IndexError, KeyError,
+                TypeError, ValueError, aiosqlite.Error, AttributeError):
+            logger.error("Ошибка выполнения процесса")
+        except KeyboardInterrupt:
+            logger.info("Принудительная остановка программы пользователем")
+            break
